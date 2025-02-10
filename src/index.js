46a578c8f8effd08e9d8aa94e34fa8cbc1a58ab3
SithPrincess <Cassie2727@gmail.com>
2025-02-09 18:50:17 -0600

    Basic html form and function, typewriter js insert

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+new Typewriter("#poem", {
+  strings: "This is a poem!",
+  autoStart: true,
+  delay: 3 ,
+  cursor: "",
+});
+
+}
+let poemForm = document.querySelector("#poem-generator-form");
+poemForm.addEventListener("submit", generatePoem);
